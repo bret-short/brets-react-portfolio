200ebfaf883902f2c83f1f91fd6be671479a2b0b
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2021-04-04 13:21:39 -0600

    setting up component folders and files

---
+++ src/components/About/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './About.css';
+import headshot from '../../assets/images/headshot.jpg';
+
+function About() {
+	return (
+		<section id='about'>
+			<h2 className='section-title'>About Me</h2>
+			<img
+				src={headshot}
+				alt='Headshot'
+				className='headshot'
+			/>
+			<h4 className='section-text'>
+				Full Stack Web Developer with over 10 years of
+				sales and customer service experience.
+				<br />
+				Experienced in delivering custom JavaScript,
+				HTML, CSS and React requests for client projects.
+				<br />
+				Knowledgeable about communicating with customers about questions concerns.
+                Problem-solver and expert critical thinker. 
+			</h4>
+		</section>
+	);
+}
+
+export default About;
